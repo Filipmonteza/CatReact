040816f4c5eb52f3d4242cd827c57fa691cf4ba7
Filip <Filipnordic@gmail.com>
2026-05-12 19:16:26 +0200

    Added filestruckture

---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Navbar from "../components/Navbar"
+
+function Home() {
+  return (
+    <>
+      <Navbar />
+
+      <main>
+        <h1>Cat Explorer</h1>
+      </main>
+    </>
+  )
+}
+
+export default Home
