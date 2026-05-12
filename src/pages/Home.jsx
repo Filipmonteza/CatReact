f3245c69bc2424ffaf1eb3c042b971da672f8aa2
Filip <Filipnordic@gmail.com>
2026-05-12 19:47:07 +0200

    Added Navbar funktions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 import Navbar from "../components/Navbar"
+import { useEffect, useState } from "react"
 
 function Home() {
+
+  const [cats, setCats] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    
+  }, [])
+
   return (
     <>
       <Navbar />
